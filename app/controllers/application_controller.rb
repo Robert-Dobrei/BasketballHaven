613447afafd71cd91cc170fe748bc2f580c29f15
Robert Dobrei <crazylord2001@gmail.com>
2023-04-22 10:23:58 +0300

    users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,19 @@ class ApplicationController < ActionController::Base
 
     def extra 
         render json: {
-            "message": "hello world"
-            "error" : "goodbye world"
+            "message": "hello world",
+            "error": "goodbye world"
         }
     end
+
+    def test
+        @data = { nume: "Test", mesaj: "Message" }
+        html_r = "HTML test"
+        
+        respond_to do |format|
+          format.html { render html: html_r }
+          format.json { render json: @data }
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   root 'application#hello'
 
   get 'goodbye', to: 'application#goodbye'
 
-  get 'extra', to 'application#goodbye'
+  get 'extra', to: 'application#extra'
+
+  get 'test', to: 'application#test'
+
+  get 'sign_up', to: 'users#new'
 end
